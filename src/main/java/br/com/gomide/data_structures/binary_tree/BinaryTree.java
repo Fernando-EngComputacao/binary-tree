24418a7fcd4ffe7564ea701b0d00244fa75799f2
davicab <davicabral01@gmail.com>
2022-09-28 22:40:13 -0300

    Update BinaryTree.java

--- src/main/java/br/com/gomide/data_structures/binary_tree/BinaryTree.java
+++ src/main/java/br/com/gomide/data_structures/binary_tree/BinaryTree.java
@@ -13,9 +13,9 @@ public class BinaryTree<T extends Comparable<T>> implements IBinaryTree<T> {
 	public Node<T> createTree(T[] elements) {
 		Node<T> n = new Node<T>();
 		n.setValue(elements[0]);
-		
+
 		for (int i = 1; i < elements.length; i++) {
-			insert(n , elements[i]);
+			insert(n, elements[i]);
 		}
 		return n;
 	}
@@ -29,14 +29,14 @@ public class BinaryTree<T extends Comparable<T>> implements IBinaryTree<T> {
 	@Override
 	public void insert(Node<T> rootNode, T element) {
 		if (rootNode.getValue().compareTo(element) > 0) {
-			if(rootNode.getLeft() == null) {
+			if (rootNode.getLeft() == null) {
 				Node<T> n = new Node<T>();
 				n.setValue(element);
 				rootNode.setLeft(n);
 			} else {
 				insert(rootNode.getLeft(), element);
 			}
-		} else if(rootNode.getValue().compareTo(element) < 0) {
+		} else if (rootNode.getValue().compareTo(element) < 0) {
 			if (rootNode.getRight() == null) {
 				Node<T> n = new Node<T>();
 				n.setValue(element);
@@ -55,37 +55,49 @@ public class BinaryTree<T extends Comparable<T>> implements IBinaryTree<T> {
 
 	@Override
 	public Node<T> getFather(Node<T> rootNode, T nodeElement) {
-		// TODO Auto-generated method stub
+		if (rootNode == null || rootNode.getValue() == nodeElement) {
+			return null;
+		}
+		if ((rootNode.getLeft() != null) && (rootNode.getLeft().getValue() == nodeElement)
+				|| (rootNode.getRight() != null) && (rootNode.getRight().getValue() == nodeElement)) {
+			return rootNode;
+		}
+		if ((rootNode.getLeft() != null)) {
+			return getFather(rootNode.getLeft(), nodeElement);
+		}
+		if ((rootNode.getRight() != null)) {
+			return getFather(rootNode.getRight(), nodeElement);
+		}
+
 		return null;
 	}
 
 	@Override
 	public Node<T> getBrother(Node<T> rootNode, T nodeElement) {
-		// TODO Auto-generated method stub
-		return null;
+		return null
 	}
 
 	@Override
 	public Node<T> getByElement(Node<T> rootNode, T element) {
-		if(rootNode.getValue().compareTo(element) == 0) {
+		if (rootNode.getValue().compareTo(element) == 0) {
 			return rootNode;
 		}
-		
-		if((rootNode.getLeft() != null) && (rootNode.getLeft().getValue().compareTo(element) >= 0)) {
+
+		if ((rootNode.getLeft() != null) && (rootNode.getLeft().getValue().compareTo(element) >= 0)) {
 			Node<T> n = getByElement(rootNode.getLeft(), element);
 			if (n != null) {
 				return n;
 			}
 		}
-		
-		if((rootNode.getRight() != null) && (rootNode.getRight().getValue().compareTo(element) <= 0)) {
+
+		if ((rootNode.getRight() != null) && (rootNode.getRight().getValue().compareTo(element) <= 0)) {
 			Node<T> n = getByElement(rootNode.getRight(), element);
-			
+
 			if (n != null) {
 				return n;
 			}
 		}
-		
+
 		return null;
 	}
 
@@ -94,18 +106,18 @@ public class BinaryTree<T extends Comparable<T>> implements IBinaryTree<T> {
 		Integer depth = 0;
 		Node<T> left = rootNode.getLeft();
 		Node<T> right = rootNode.getRight();
-		
-		if(left != null) {
+
+		if (left != null) {
 			Integer n = calculateTreeDepth(rootNode.getLeft());
-			depth = depth > n ? depth: n;
+			depth = depth > n ? depth : n;
 		}
-		
-		if(right != null) {
+
+		if (right != null) {
 			Integer n = calculateTreeDepth(rootNode.getRight());
-			depth = depth > n ? depth: n;
+			depth = depth > n ? depth : n;
 		}
-		
-		return (right == null) && (left == null) ? 0: ++depth;
+
+		return (right == null) && (left == null) ? 0 : ++depth;
 	}
 
 	@Override
@@ -117,24 +129,23 @@ public class BinaryTree<T extends Comparable<T>> implements IBinaryTree<T> {
 	@Override
 	public String toString(Node<T> rootNode) {
 		String result = "";
-		result += rootNode.getValue().toString() + " ";//TODO VERIFICAR QUANDO O VALUE É DE UM NO ROOT COM getFather()
-		
-		Node<T> left  = rootNode.getLeft();
+		result += rootNode.getValue().toString() + " ";// TODO VERIFICAR QUANDO O VALUE É DE UM NO ROOT COM getFather()
+
+		Node<T> left = rootNode.getLeft();
 		Node<T> right = rootNode.getRight();
-		
-		result = ((left != null) || (right != null)) ? result + "(": result;
-		
-		if(left != null) {
+
+		result = ((left != null) || (right != null)) ? result + "(" : result;
+
+		if (left != null) {
 			result += "left:" + toString(rootNode.getLeft());
 		}
-		
-		if(right != null) {
+
+		if (right != null) {
 			result += "right:" + toString(rootNode.getRight());
 		}
-		
-		result = ((left != null) || (right != null)) ? result + ")": result;
 
-		
+		result = ((left != null) || (right != null)) ? result + ")" : result;
+
 		return result;
 	}
 
